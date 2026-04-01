a2ac4c45ea1ddc1481463ee8afad03255fbc3dd3
Julia Khanyukova <julia.khanyukova@gmail.com>
2026-04-01 14:20:42 -0400

    addnew.html layout finalizations, current profile display, dark mode working

--- js/addnew.js
+++ js/addnew.js
@@ -1,3 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    applyTheme();
+    loadProfile();
+});
+
+function loadProfile() {
+    const currentProfile = document.getElementById("currentProfile");
+
+    const avatar = localStorage.getItem("profileAvatar");
+    const username = localStorage.getItem("profileUsername");
+
+    if (!avatar || !username) return;
+
+    currentProfile.innerHTML = `
+        <div class="current-profile-card">
+            <img src="${avatar}" alt="${username}">
+            <div class="current-profile-info">
+                <p>${username}</p>
+            </div>
+        </div>
+    `;
+}
+
+function applyTheme() {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    document.body.classList.toggle("dark-mode", savedTheme === "dark");
+}
+
+
+
 const input = document.getElementById("imageInput");
 const preview = document.getElementById("preview");
 const label = document.getElementById("imageLabel");
@@ -20,3 +50,4 @@ document.querySelector(".ingredients .placeholder")
         e.preventDefault();
     }
 });
+
